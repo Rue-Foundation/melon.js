64620f95cbc603e4789f68ce954d072195e39f39
Jenna Zenk <zenkjenna@gmail.com>
2017-11-20 15:31:54 +0100

    wip

--- tests/integration/setup.js
+++ tests/integration/setup.js
@@ -1,16 +1,25 @@
 require("dotenv").config();
 
 /* eslint-disable import/first */
-import Web3 from "web3";
+// import Web3 from "web3";
 import setup from "../../lib/utils/setup";
+import ethers from "ethers";
+
 /* eslint-enable */
 
-const web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+// const web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+
+const provider = new ethers.providers.JsonRpcProvider(
+  "http://localhost:8545",
+  "kovan",
+);
+
 try {
   setup.init({
-    web3,
+    provider,
     daemonAddress: "0x00360d2b7d240ec0643b6d819ba81a09e40e5bcd",
-    defaultAccount: process.env.UNLOCKED_ACCOUNT,
+    // defaultAccount: process.env.UNLOCKED_ACCOUNT,
+    defaultAccount: "0x2809a8B74D51eEcFE4d229a47937964e43D55c30",
     tracer: ({ timestamp, message, category, data }) => {
       const args = [timestamp.toISOString(), `[${category}]`, message];
       if (category === "ensureFailed") {
